cb4637f1d9ee89087cbde421903ffb535c60ec1b
yatyat2 <xowls2287@naver.com>
2022-01-20 22:49:39 +0900

    초기 셋팅

---
+++ src/main/java/com/example/springmvchandlebars/SpringMvcHandlebarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvchandlebars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHandlebarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcHandlebarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvchandlebars/SpringMvcHandlebarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvchandlebars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHandlebarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
